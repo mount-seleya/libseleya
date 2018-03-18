8a72ba6ad0a957201db91dc66ead814a941b95ed
crito <crito@fnordpipe.org>
2018-03-18 12:50:39 +0100

    implement monitor modes

--- src/include/seleya/monitor.h
+++ src/include/seleya/monitor.h
@@ -7,14 +7,25 @@
 #include <GLFW/glfw3.h>
 
 typedef struct seleya_monitor_t seleya_monitor_t;
+typedef struct seleya_monitor_mode_t seleya_monitor_mode_t;
 
 struct seleya_monitor_t {
   GLFWmonitor *monitor;
   const char *name;
   int primary;
+  seleya_monitor_mode_t *modes;
   seleya_monitor_t *next;
 };
 
+struct seleya_monitor_mode_t {
+  int w;
+  int h;
+  int current;
+  int refresh_rate;
+  seleya_monitor_mode_t *next;
+};
+
 seleya_monitor_t *seleya_monitor_new(GLFWmonitor *m);
+seleya_monitor_mode_t *seleya_monitor_mode_new(GLFWmonitor *m, const GLFWvidmode *vm);
 
 #endif
--- src/libseleya_monitor.c
+++ src/libseleya_monitor.c
@@ -14,6 +14,9 @@ seleya_monitor_t *seleya_monitor_new(GLFWmonitor *m)
   seleya_monitor_t *monitor = NULL;
   GLFWmonitor *primary = NULL;
   const char *primary_name = NULL;
+  const GLFWvidmode *vm = NULL;
+  seleya_monitor_mode_t *mode_next = NULL;
+  int c, i;
 
   if(m)
   {
@@ -22,6 +25,7 @@ seleya_monitor_t *seleya_monitor_new(GLFWmonitor *m)
       return NULL;
 
     memset(monitor, 0, sizeof(seleya_monitor_t));
+    monitor->monitor = m;
     monitor->name = glfwGetMonitorName(m);
 
     primary = glfwGetPrimaryMonitor();
@@ -29,7 +33,49 @@ seleya_monitor_t *seleya_monitor_new(GLFWmonitor *m)
 
     if(strcmp(monitor->name, primary_name) == 0)
       monitor->primary = 1;
+
+    vm = glfwGetVideoModes(m, &c);
+    for(i = 0; i <= c; ++i)
+    {
+      mode_next = seleya_monitor_mode_new(m, &vm[i]);
+      if(mode_next)
+      {
+        if(monitor->modes)
+          mode_next->next = monitor->modes;
+
+        monitor->modes = mode_next;
+      }
+    }
   }
 
   return monitor;
+}
+
+seleya_monitor_mode_t *seleya_monitor_mode_new(GLFWmonitor *m, const GLFWvidmode *vm)
+{
+  seleya_monitor_mode_t *mode = NULL;
+  const GLFWvidmode *current = NULL;
+
+  if(vm)
+  {
+    mode = malloc(sizeof(seleya_monitor_mode_t));
+    if(!mode)
+      return NULL;
+
+    memset(mode, 0, sizeof(seleya_monitor_mode_t));
+    mode->w = vm->width;
+    mode->h = vm->height;
+    mode->refresh_rate = vm->refreshRate;
+
+    current = glfwGetVideoMode(m);
+    if(current && current->width == vm->width &&
+        current->height == vm->height &&
+        current->refreshRate == vm->refreshRate &&
+        current->redBits == vm->redBits &&
+        current->greenBits == vm->greenBits &&
+        current->blueBits == vm->blueBits)
+      mode->current = 1;
+  }
+
+  return mode;
 }
